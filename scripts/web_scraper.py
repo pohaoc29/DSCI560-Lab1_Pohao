b7538bb966afef1d4340dc13a5c2fcbfcd169937
pohaoc29 <pohaoc@usc.edu>
2025-08-30 16:08:36 -0700

    fourth commit
    add selenium version web scrapper to get market banner data and generate two html: with banner and no banner

--- scripts/web_scraper.py
+++ scripts/web_scraper.py
@@ -24,13 +24,13 @@ markets = market_banner.find_all("a")
 
 #print(market_banner.prettify())  # print the market banner HTML
 # write into web_data.html file
-with open('../data/raw_data/web_data.html', 'a') as file:
-    file.write(market_banner.prettify())
+with open('../data/raw_data/web_data_no_banner.html', 'a') as f:
+    f.write(market_banner.prettify())
 
 # Letest News Section
 latest_news_section = soup.find('div', class_="LatestNews-isHomePage LatestNews-isIntlHomepage")
 
 #print(latest_news_section.prettify())  # print the latest news HTML
 # write into web_data.html file
-with open('../data/raw_data/web_data.html', 'a') as file:
-    file.write(latest_news_section.prettify())
+with open('../data/raw_data/web_data_no_banner.html', 'a') as f:
+    f.write(latest_news_section.prettify())
---
+++ scripts/web_scraper_selenium.py
@@ -0,0 +1,81 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
+# Selenium Attributes
+options = Options()
+options.add_argument("--headless")  # Not opening the browser window
+options.add_argument("--width=1920")
+options.add_argument("--height=1080")
+
+service = Service("/snap/bin/geckodriver")  # manually assign geckodriver to void errors
+
+driver = webdriver.Firefox(service=service, options=options)
+url = "https://www.cnbc.com/world/?region=world"
+
+#print("[INFO] Opening page...")
+driver.get(url)
+
+
+# Market Banner Section
+# Wait for market banner loading
+try:
+    # Wait for at least one MarketCard-container <a> occur
+    WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#market-data-scroll-container a.MarketCard-container"))
+    )
+    #print("[INFO] Market Banner data loaded successfully!")
+except:
+    print("[ERROR] Timeout: Market Banner data not found.")
+    driver.quit()
+    exit()
+
+
+
+
+# Get HTML
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
+
+
+
+# Find the market banner part
+banner = soup.find("div", id="market-data-scroll-container")
+
+if not banner:
+    print("[WARNING] Market Banner not found!")
+    driver.quit()
+    exit()
+
+#print(banner.prettify())
+
+
+# Write into file
+raw_path = "../data/raw_data/web_data.html"
+with open(raw_path, "a") as f:
+    f.write(banner.prettify())
+#print(f"[INFO] Market Banner HTML saved to {raw_path[:-13]}")
+
+
+
+# Latest News Section
+latest_news_section = soup.find('div', class_="LatestNews-isHomePage LatestNews-isIntlHomepage")
+
+if not latest_news_section:
+    print("[WARNING] Latest News not found!")
+    driver.quit()
+    exit()
+
+#print(latest_news_section.prettify())
+
+# Write into file
+with open(raw_path, "a") as f:
+    f.write(latest_news_section.prettify())
+#print(f"[INFO] Latest News HTML saved to {raw_path[:-13]}")
+
+
+driver.quit()
